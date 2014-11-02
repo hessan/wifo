811758f4988402a0f433415366707e402a6f40aa
Hessan Feghhi <hessan@annahid.com>
2014-11-02 11:45:29 +0000

    Documented RecordList

--- WiFo/Data/Record.cs
+++ WiFo/Data/Record.cs
@@ -5,13 +5,10 @@ namespace WiFo.Data
 	/// <summary>
 	/// Represents a single status change event, which contains a timestamp and the new value of the Broadcom bcm43xx status register.
 	/// </summary>
-	public sealed class Record : IComparable
+	public sealed class Record : IComparable<Record>
 	{
-		private uint time;
-		private uint ifs_state;
-
 		/// <summary>
-		/// Initializes a new instance of the Record with given timestamp and status.
+		/// Initializes a new instance of the <see cref="Record"/> with given timestamp and status.
 		/// </summary>
 		/// <param name="time">The timestamp associated with this record</param>
 		/// <param name="state">The value of the status register that this record represents</param>
@@ -54,10 +51,29 @@ namespace WiFo.Data
 			}
 		}
 
-		public int CompareTo(object obj)
+		/// <summary>
+		/// Determines whether the specified object is a <see cref="Record"/> and is equal to the current object.
+		/// </summary>
+		/// <param name="obj">The other object.</param>
+		/// <returns><b>true</b> if the specified object is equal to the current object; otherwise, <b>false</b>.</returns>
+		public override bool Equals(object obj)
 		{
-			Record other = obj as Record;
+			Record rec = obj as Record;
 
+			if(rec == null)
+				return false;
+
+			return rec.time == time && rec.ifs_state == ifs_state;
+		}
+
+		/// <summary>
+		/// Compares the current instance with another <see cref="Record"/> object and returns an integer that indicates
+		/// whether the current instance precedes, follows, or occurs at the same time as the other object.
+		/// </summary>
+		/// <param name="other">The <see cref="Record"/> object to compare with this instance.</param>
+		/// <returns>A value that indicates the relative order of the objects being compared.</returns>
+		public int CompareTo(Record other)
+		{
 			if (other == null)
 				return 1;
 
@@ -70,24 +86,26 @@ namespace WiFo.Data
 			return 0;
 		}
 
-		public override bool Equals(object obj)
-		{
-			Record rec = obj as Record;
-
-			if(rec == null)
-				return false;
-
-			return rec.time == time && rec.ifs_state == ifs_state;
-		}
-
+		/// <summary>
+		/// Serves as the hash function for <see cref="Record"/> objects.
+		/// </summary>
+		/// <returns>The hash code based on the timestamp.</returns>
 		public override int GetHashCode()
 		{
 			return (int)time;
 		}
 
+		/// <summary>
+		/// Returns a string that represents the current instance.
+		/// </summary>
+		/// <returns>A string that represents the current object.</returns>
+		/// <value>This is typically the string representation of the <see cref="State"/> property.</value>
 		public override string ToString()
 		{
 			return ifs_state.ToString();
 		}
+
+		private uint time;
+		private uint ifs_state;
 	}
 }
--- WiFo/Data/RecordList.cs
+++ WiFo/Data/RecordList.cs
@@ -3,21 +3,54 @@ using System.Collections.Generic;
 
 namespace WiFo.Data
 {
-	public class RecordList : IEnumerable<Record>
+	/// <summary>
+	/// <para>Represents a special-purpose enumerable list of <see cref="Record"/> objects.</para>
+	/// </summary>
+	/// <remarks>
+	/// <para>The list only supports addition and purging, and individual items cannot be deleted from it.</para>
+	/// <para>
+	/// Adding elements is performed in batches rather than single items. This behavior is
+	/// optimized for WiFo client to fetch and consume data efficiently from a server.
+	/// </para>
+	/// </remarks>
+	
+	public class RecordList : IEnumerable<Record>, System.Collections.IEnumerable
 	{
-		private List<RecordPage> pages = null;
-		private int listCount;
-		private int capacity;
-
-		private const int pageSize = 512;
-
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecordList"/> with the specified capacity.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// The capacity specified in this constructor is the maximum desired number of items that can be
+		/// stored in this <see cref="RecordList"/> instance. The list will never hold more items than this
+		/// capacity.
+		/// </para>
+		/// <para>
+		/// After reaching the capacity, the list turns into a ring buffer, and adding more items causes older
+		/// records to be purged. Purging old records is performed in batches. Therefore, a ring buffer typically
+		/// holds less than the capacity.
+		/// </para>
+		/// <para>
+		/// The design of the <see cref="RecordList"/> class and typical batch size prohibits capacities of less
+		/// than 512. To comply with this, this constructor ignores lower capacities specified, using 512 instead.
+		/// </para>
+		/// </remarks>
+		/// <param name="capacity">The desired capacity of the list.</param>
 		public RecordList(int capacity)
 		{
-			capacity = Math.Max(10, capacity);
+			capacity = Math.Max(512, capacity);
 			this.capacity = capacity;
 			this.listCount = 0;
 		}
 
+		/// <summary>
+		/// Gets the first record in the list.
+		/// </summary>
+		/// <remarks>
+		/// To gain the best performance regardless of specific implementation details in current and future
+		/// releases, it is recommended to use this property instead of using the subscript operator when
+		/// accessing the first element of the list.
+		/// </remarks>
 		public Record FirstRecord
 		{
 			get
@@ -26,6 +59,14 @@ namespace WiFo.Data
 			}
 		}
 
+		/// <summary>
+		/// Gets the last record in the list.
+		/// </summary>
+		/// <remarks>
+		/// To gain the best performance regardless of specific implementation details in current and future
+		/// releases, it is recommended to use this property instead of using the subscript operator when
+		/// accessing the last element of the list.
+		/// </remarks>
 		public Record LastRecord
 		{
 			get
@@ -34,20 +75,50 @@ namespace WiFo.Data
 			}
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Capacity
+		{
+			get
+			{
+				return capacity;
+			}
+		}
+
+		/// <summary>
+		/// Gets the number of items currently in the list.
+		/// </summary>
+		/// <remarks>
+		/// This value is equal to <see cref="Capacity" />, but it is typically less (see <see cref="RecordList(int)" />).
+		/// </remarks>
 		public virtual int Count
 		{
 			get { return listCount; }
 		}
 
-		public virtual Record this[int ix]
+		/// <summary>
+		/// Gets the <see cref="Record"/> at the specified index.
+		/// </summary>
+		/// <param name="index">The zero-based index of the element to get.</param>
+		/// <returns>The record at the specified index.</returns>
+		public virtual Record this[int index]
 		{
 			get
 			{
-				int pageIndex = ix / pageSize, pageOffset = ix % pageSize;
+				int pageIndex = index / pageSize, pageOffset = index % pageSize;
 				return pages[pageIndex][pageOffset];
 			}
 		}
 
+		/// <summary>
+		/// Appends all items in the given range to the list.
+		/// </summary>
+		/// <param name="records">The enumerable range of elements to add.</param>
+		/// <remarks>
+		/// This method is intended for internal use, and should not be called by extensions.
+		/// </remarks>
+		/// <seealso cref="WiFo.Extensibility.IExtension" />
 		public virtual void AppendAll(IEnumerable<Record> records)
 		{
 			if (pages == null)
@@ -79,6 +150,9 @@ namespace WiFo.Data
 			}
 		}
 
+		/// <summary>
+		/// Clears this list, purging all records.
+		/// </summary>
 		public virtual void Clear()
 		{
 			if(pages != null)
@@ -87,41 +161,68 @@ namespace WiFo.Data
 			listCount = 0;
 		}
 
-		public int NextChange(int startIndex, int mask)
+		/// <summary>
+		/// Finds the next record in the list where the masked value of the <see cref="State"/> property
+		/// is changed.
+		/// </summary>
+		/// <param name="startIndex">The starting index.</param>
+		/// <param name="mask">The mask to be tracked.</param>
+		/// <returns>The index of the next change.</returns>
+		public int NextChange(int startIndex, uint mask)
 		{
 			if (startIndex < 0)
 				return -1;
 
-			int comp = (int)this[startIndex].State & mask;
+			uint comp = this[startIndex].State & mask;
 
 			for (int i = startIndex + 1; i < Count; i++)
-				if (comp != ((int)this[i].State & mask))
+				if (comp != (this[i].State & mask))
 					return i;
 
 			return -1;
 		}
 
-		public int PrevChange(int startIndex, int mask)
+		/// <summary>
+		/// Searching backwards, finds the next record in the list where the masked value of the <see cref="State"/>
+		/// property is changed.
+		/// </summary>
+		/// <param name="startIndex">The starting index.</param>
+		/// <param name="mask">The mask to be tracked.</param>
+		/// <returns>The index of the next change.</returns>
+		public int PrevChange(int startIndex, uint mask)
 		{
 			if (startIndex < 0)
 				return -1;
 
-			int comp = (int)(this[startIndex].State & mask);
+			uint comp = (this[startIndex].State & mask);
 
 			for (int i = startIndex - 1; i >= 0; i--)
-			{
-				if (comp != (int)(this[i].State & mask))
+				if (comp != (this[i].State & mask))
 					return i;
-			}
 
 			return -1;
 		}
 
+		/// <summary>
+		/// Returns a <see cref="RecordList"/> instance, containing a subrange of the records in the
+		/// current instance.
+		/// </summary>
+		/// <remarks>
+		/// You may assume that the new list allcoates constant memory, which is independent of the number
+		/// of items or the capacity of the current list.
+		/// </remarks>
+		/// <param name="startIndex">The index which marks the beginning of the new list.</param>
+		/// <param name="endIndex">The end of the new list (exclusive of this index).</param>
+		/// <returns>The cropped list.</returns>
 		public RecordList Crop(int startIndex, int endIndex)
 		{
 			return new CroppedRecordList(this, startIndex, endIndex);
 		}
 
+		/// <summary>
+		/// Returns an enumerator that iterates through the records in this list.
+		/// </summary>
+		/// <returns>An IEnumerator object that can be used to iterate through the collection.</returns>
 		public virtual IEnumerator<Record> GetEnumerator()
 		{
 			foreach(RecordPage page in pages)
@@ -131,7 +232,12 @@ namespace WiFo.Data
 
 		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
 		{
-			return GetEnumerator();
+			throw new NotImplementedException();
 		}
+
+		private const int pageSize = 512;
+		private List<RecordPage> pages = null;
+		private int listCount;
+		private int capacity;
 	}
 }
